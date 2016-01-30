5dacc71ede2c0328e5324bd39779d842a0e346ce
Jarno Elovirta <jarno@elovirta.com>
2016-01-30 14:08:33 +0200

    Initial draft of TOC search

--- js/app/main.js
+++ js/app/main.js
@@ -1,1 +1,1 @@
-'use strict';define(['TocController'],function(TocController){TocController()});
+'use strict';define(['TocController','SearchController','jquery'],function(TocController,SearchController,$){var indexAttr=$("link[rel=index]").attr('href');if(indexAttr&&window.history){var index=new URI(indexAttr).absoluteTo(window.location.href).href();$.ajax({url:index,success:load})}function load(data){var $toc=$("<body>").append($.parseHTML(data)).find('nav');TocController($toc);SearchController($toc)}});
